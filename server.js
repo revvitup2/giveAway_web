52220d5931892e45703f2f21c5c2daf68684cde9
shilpabhansali <shilpa.bhansali@bt.com>
2021-01-28 12:47:07 +0000

    ui change

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express=require("express")
-
-const app=express();
-app.use(express.static("public"));
+var express = require("express");
+ 
+var app = express();
+	
+app.use(express.static('public'));
 app.use('/css', express.static(__dirname + '/public/assets/css'));
 app.use('/js', express.static(__dirname + '/public/assets/js'));
 app.use('/images', express.static(__dirname + '/public/assets/images'));
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/index.html");
-})
 
-app.listen(8080,function(){
-    console.log("running at 8080");
-})
+var server = app.listen(8081, function(){
+    var port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
+
