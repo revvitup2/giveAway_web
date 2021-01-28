c26d1b90b0bdf4fd5aa1da71717c575ca124104f
dhruv mittal <dhruvmittal1910@gmail.com>
2021-01-28 17:59:25 +0530

    changes to index.html

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express=require("express")
 
 const app=express();
 app.use(express.static("public"));
-
+app.use('/css', express.static(__dirname + '/public/assets/css'));
+app.use('/js', express.static(__dirname + '/public/assets/js'));
+app.use('/images', express.static(__dirname + '/public/assets/images'));
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
